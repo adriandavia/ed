f8c09d2ec519a9a1c162212fa0da86933d27a6ec
adriandavia15 <dam.adavia@institutoserpis.org>
2015-10-26 12:13:38 +0100

    Array

--- JHolaMundo/src/org/institutoserpis/ed/Array.java
+++ JHolaMundo/src/org/institutoserpis/ed/Array.java
@@ -2,25 +2,36 @@ package org.institutoserpis.ed;
 
 public class Array {
 	public static void main (String [] args){
-		int[] v = new int[]{64, 9, 7, 12, 21};
+		int[] v = new int[]{3, 9, 7, 11, 21};
 		for (int index=0; index < 5; index++)
 			System.out.println ("elemento con index: "+index+"vale"+v[index]);
 		System.out.println ("suma del array= "+suma(v));
 		System.out.println ("el valor menor en v: " +menor(v));
 	}
-	private static int suma(int[] v){ // numero de elementos del vector: v.legth
+	public static int suma(int[] v){ // numero de elementos del vector: v.legth
 		int suma;
 		suma = 0;
-		for (int index=0; index < v.length; index++)
-			suma = suma + v[index];
+		for (int item : v)
+			suma = suma + item;
 		return suma;
 	}
-	private static int menor(int[] v){
+	
+	public static int menor(int[] v){
 		int menor;
 		menor = v[0];
-		for(int numero=0; numero<=v[]; numero++)
-			menor = numero;
+		for(int index=0; index<=v.length; index++)
+			if (v[index]<menor)
+				menor = v[index];
 		return menor;
+		}
+	
+	public static int indexOf(int[] v, int item){
+		int index = 0;
+		while (index <v.length && v[index]!=item)
+			index++;
+		if (index == v.length)
+			return -1;
+		return index;
 	}
 }
 
---
+++ JHolaMundo/src/org/institutoserpis/ed/ArrayTest.java
@@ -0,0 +1,24 @@
+package org.institutoserpis.ed;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayTest extends Array {
+
+	@Test
+	public void indexOf() {
+		assertEquals (0, Array.indexOf(new int[]{7,21,9,13}, 7));
+		assertEquals (1, Array.indexOf(new int[]{7,21,9,13}, 21));
+		assertEquals (3, Array.indexOf(new int []{7,21,9,13}, 13));
+	}
+
+	@Test
+	public void testMenor() {
+		//fail("Not yet implemented");
+		assertEquals (5,Array.menor(new int []{5,9,6,14}));
+		assertEquals (3,Array.menor(new int []{5,9,3,14}));
+		assertEquals (7,Array.menor(new int []{9,8,12,7}));
+	}
+
+}
