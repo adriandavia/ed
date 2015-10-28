7348a7bb5124f08606750da7c47c0053ec6207a5
adriandavia15 <dam.adavia@institutoserpis.org>
2015-10-28 08:47:49 +0100

    ArrayTest

--- JHolaMundo/src/org/institutoserpis/ed/Array.java
+++ JHolaMundo/src/org/institutoserpis/ed/Array.java
@@ -2,9 +2,9 @@ package org.institutoserpis.ed;
 
 public class Array {
 	public static void main (String [] args){
-		int[] v = new int[]{3, 9, 7, 11, 21};
+		int[] v = new int[]{3, 9, 7, 1, 21};
 		for (int index=0; index < 5; index++)
-			System.out.println ("elemento con index: "+index+"vale"+v[index]);
+			System.out.println ("elemento con index: "+index+" vale "+v[index]);
 		System.out.println ("suma del array= "+suma(v));
 		System.out.println ("el valor menor en v: " +menor(v));
 	}
@@ -19,19 +19,19 @@ public class Array {
 	public static int menor(int[] v){
 		int menor;
 		menor = v[0];
-		for(int index=0; index<=v.length; index++)
-			if (v[index]<menor)
+		for(int index = 1; index < v.length; index++)
+			if (v[index] < menor)
 				menor = v[index];
 		return menor;
 		}
 	
 	public static int indexOf(int[] v, int item){
-		int index = 0;
-		while (index <v.length && v[index]!=item)
-			index++;
-		if (index == v.length)
-			return -1;
-		return index;
+		for (int index =0; index < v.length; index++){
+			if (v[index] == item){
+				return index;
+			}
+		}
+		return -1;
 	}
 }
 
--- JHolaMundo/src/org/institutoserpis/ed/ArrayTest.java
+++ JHolaMundo/src/org/institutoserpis/ed/ArrayTest.java
@@ -11,6 +11,10 @@ public class ArrayTest extends Array {
 		assertEquals (0, Array.indexOf(new int[]{7,21,9,13}, 7));
 		assertEquals (1, Array.indexOf(new int[]{7,21,9,13}, 21));
 		assertEquals (3, Array.indexOf(new int []{7,21,9,13}, 13));
+		assertEquals (-1, Array.indexOf (new int [] {7,21,9,13}, 33));
+	}
+	public void indexOf_empty(){
+		assertEquals (-1, Array.indexOf(new int[]{}, 33));
 	}
 
 	@Test
