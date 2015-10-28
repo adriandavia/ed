ec3b2bc32f2cdd882bc0394cbc76575aa606c6eb
adriandavia15 <dam.adavia@institutoserpis.org>
2015-10-28 08:51:29 +0100

    Arreglado

---
+++ JHolaMundo/src/HolaMundo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HolaMundo{
+	public static void main(String[] args) {
+		System.out.print("Introduce tu nombre: ");
+		Scanner scanner = new Scanner(System.in);
+		String nombre = scanner.nextLine();
+		
+		System.out.printf("Hola %s desde eclipse", nombre);
+
+	}
+
+}
